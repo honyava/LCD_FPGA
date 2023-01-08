5ce514d6ccf74ecbdc3cedd082230a5c5bfd1e4e
honyava <88489271+honyava@users.noreply.github.com>
2023-01-08 19:31:46 +0300

    END
    END

--- output_files/ControllerTest_TOP.vhd
+++ output_files/ControllerTest_TOP.vhd
@@ -2,6 +2,7 @@ library IEEE;
 use IEEE.std_logic_1164.all;
 use IEEE.std_logic_unsigned.all;
 use ieee.numeric_std.all;
+use ieee.math_real.all;
 
 ENTITY ControllerTest_TOP IS
     PORT(
@@ -23,8 +24,8 @@ ARCHITECTURE behavior OF ControllerTest_TOP IS
     --    constant ip_addr   : std_logic_vector(31 downto 0) := X"FFA8010A";
     signal   massive_lcd : STD_LOGIC_VECTOR(31 DOWNTO 0) := (others => '0');
     signal   changed : std_logic := '0';
-    signal   global_flag_start : integer RANGE 0 TO 1 := 0;
-    signal   global_flag_finish : integer RANGE 0 TO 1 := 0;
+    signal   global_flag_start : integer := 0;
+    signal   global_flag_finish : integer := 0;
     
     function bindec_func(SW : STD_LOGIC_VECTOR; N : integer) return std_logic_vector is
         variable z: STD_LOGIC_VECTOR (N*2+N/4 downto 0) := (others => '0');
@@ -87,13 +88,13 @@ BEGIN
     
     process(clk)
         variable temp_ip : std_logic_vector(7 downto 0) := X"00";
-        variable cnt : integer RANGE 0 TO 33 := 0;
+        variable cnt : integer := 0;
         variable bindec : std_logic_vector(11 downto 0) := (others => '0');
         variable div_freq_lcd : natural := 0;
-        variable send_flag : integer RANGE 0 TO 1 := 0;
-        variable flag_zero : integer RANGE 0 TO 1 := 0;   --flag for sending point  (.)
-        variable j : integer RANGE 0 TO 10 := 0;
-        variable k : integer RANGE 0 TO 10 := 0;
+        variable send_flag : integer := 0;
+        variable flag_point : integer := 0;   --flag for sending point  (.)
+        variable j : integer := 0;
+        variable k : integer := 0;
         variable change : std_logic := '0'; -- flag for arriving new data
     begin
         if (rising_edge(clk)) then
@@ -115,21 +116,21 @@ BEGIN
             end if;
             if (div_freq_lcd > 2499 and send_flag = 1) then
                 global_flag_start <= 1;
-                if (j = 3 or flag_zero = 1) then
+                if (j = 3 or flag_point = 1) then
                     to_lcd <= X"0A"; --send point
-                    flag_zero := 0;
+                    flag_point := 0;
                     j := 3;
                 else
-                    if bindec(11 downto 8) = X"0" and flag_zero = 0 then
-                        if bindec(7 downto 4) = X"0" and flag_zero = 0 then
-                            flag_zero := 1; 
+                    if (bindec(11 downto 8) = X"0" and flag_point = 0) then
+                        if (bindec(7 downto 4) = X"0" and flag_point = 0) then
+                            flag_point := 1; 
                             to_lcd <= X"0" & bindec(3 downto 0);      --send 1 numbers of ip (1 for example)
                         else
                             if j < 2 then
                                 to_lcd <= X"0" & bindec(3 + 4*j downto 0 + 4*j);  --send 2 numbers of ip (10 for example)
-                                flag_zero := 0;
+                                flag_point := 0;
                                 if j = 1 then 
-                                    flag_zero := 1;
+                                    flag_point := 1;
                                 end if;   
                             end if;  
                         end if;
@@ -139,7 +140,7 @@ BEGIN
                 end if;
                 div_freq_lcd := 0;
                 j := j + 1;
-            elsif (j = 4) then
+            elsif j = 4 then
                 if k = 3 then
                     to_lcd <= X"0F";  --end of message
                     k := 0;
